a51612ab2e3863f1982c2188b2d89145772db5b0
Denis Menshikov <34773356+DenisMenshikov@users.noreply.github.com>
2019-03-18 13:57:25 -0400

    Create rose.js

---
+++ rose.js
@@ -0,0 +1,58 @@
+var d = 8;
+var n = 5;
+var sliderD;
+var sliderN;
+
+function setup() {
+  createCanvas(400, 400);
+  sliderD = createSlider(1, 20, 10, 1);
+  sliderN = createSlider(1, 20, 10, 1);
+  sliderD.input(draw);
+  sliderN.input(draw);
+}
+
+var Flower = function(x, y){
+    this.x = x;
+    this.y = y;
+};
+
+Flower.prototype.grow = function(){
+
+};
+
+
+
+	beginShape();
+  stroke(255);
+  noFill();
+  strokeWeight(1);
+  for (var a = 0; a < TWO_PI * reduceDenominator(n, d); a += 0.02) {
+    var r = 200 * cos(k * a);
+    var x = (r+15) * cos(a);
+    var y = (r*1.2) * sin(a);
+    vertex(x+sin(x^2), y);
+  }
+  endShape(CLOSE);
+	
+};
+
+function draw() {
+  d = sliderD.value();
+  n = sliderN.value();
+  var k = n / d;
+  background(51);
+  push();
+  translate(width / 2, height / 2);
+  
+	var rose1 = new Rose(100, 100);
+	
+	pop();
+  noLoop();
+}
+
+function reduceDenominator(numerator, denominator) {
+    function rec(a, b) {
+        return b ? rec(b, a % b) : a;
+    }
+    return denominator / rec(numerator, denominator);
+}
